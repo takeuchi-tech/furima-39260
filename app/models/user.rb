99a272a036cf92df6c931251a3274c9dc3c1a4de
takeuchi-tech <mt.tech00885@gmail.com>
2023-10-04 15:51:56 +0900

    バリデーションの記述&会員登録ができる

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :items
+  has_many :purchases
+
+  validates :nickname,        presence: true
+  validates :last_name,       presence: true
+  validates :first_name,      presence: true
+  validates :last_name_kana,  presence: true
+  validates :first_name_kana, presence: true
+  validates :date_of_birth,   presence: true
+
+end
