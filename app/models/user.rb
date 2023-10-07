173f1c8980c19dfe36bed330bb6b418f40631270
takeuchi-tech <mt.tech00885@gmail.com>
2023-10-07 12:41:42 +0900

    レビュー修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :items
-  has_many :purchases
 
   validates :nickname,        presence: true
-  validates :password,        presence: true,
-                              format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9])/, message: 'is invalid. Include both letters and numbers.' }
+  validates :password,
+                              format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers.' }
   validates :last_name,       presence: true,
-                              format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'is invalid. Input full-width characters' }
+                              format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
   validates :first_name,      presence: true,
-                              format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'is invalid. Input full-width characters' }
+                              format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
   validates :last_name_kana,  presence: true,
-                              format: { with: /\A[ァ-ン]+\z/, message: 'is invalid. Input full-width katakana characters' }
+                              format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' }
   validates :first_name_kana, presence: true,
-                              format: { with: /\A[ァ-ン]+\z/, message: 'is invalid. Input full-width katakana characters' }
+                              format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' }
   validates :date_of_birth,   presence: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 4)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {'a1'+ Faker::Internet.password(min_length: 4)}
     password_confirmation {password}
     last_name             {'田中'}
     first_name            {'太郎'}
