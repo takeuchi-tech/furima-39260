30cbc06fee3f5163125460f9486aef1e12c83277
takeuchi-tech <mt.tech00885@gmail.com>
2023-10-08 18:02:14 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,7 +19,7 @@ class Item < ApplicationRecord
 
 end
 
-class Article < ApplicationRecord
+class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :product_condition
