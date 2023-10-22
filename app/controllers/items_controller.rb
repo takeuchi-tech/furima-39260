107bd11cfb4cab3db9be3f17f8fe94947b9a4ef7
takeuchi-tech <mt.tech00885@gmail.com>
2023-10-23 01:49:39 +0900

    修正3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, except: [:index]
   before_action :move_to_index, except: [:index, :show,]
 
   def index
